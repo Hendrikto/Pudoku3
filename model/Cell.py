ff385890661c6a810aa31ca76c2e8a83e1e93753
Hendrikto <hendrik.to@gmail.com>
2016-09-01 19:03:25 +0200

    Add a class to represents cells.

---
+++ model/Cell.py
@@ -0,0 +1,22 @@
+# author: Hendrik Werner
+class Cell:
+    value = 0
+
+    def __init__(self, sudoku, row, column, block):
+        self.sudoku = sudoku
+        self.row = row
+        self.column = column
+        self.block = block
+
+    def set_value(self, new_value):
+        assert new_value in range(1, 10)
+        self.row.remove(self.value)
+        self.column.remove(self.value)
+        self.block.remove(self.value)
+
+    def clear(self):
+        if self.value:
+            self.row.add(self.value)
+            self.column.add(self.value)
+            self.block.add(self.value)
+            self.value = 0
