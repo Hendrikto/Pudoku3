a4b23695b4e9a79d5e8d275292fba415916ed044
Hendrikto <hendrik.to@gmail.com>
2016-09-02 22:10:15 +0200

    Use sets to store candidates.

--- model/Sudoku.py
+++ model/Sudoku.py
@@ -10,12 +10,12 @@ class Sudoku:
         columns = []
         blocks = []
         for i in range(9):
-            rows.append([*range(1, 10)])
-            columns.append([*range(1, 10)])
+            rows.append({*range(1, 10)})
+            columns.append({*range(1, 10)})
         for x in range(3):
             blocks.append([])
             for y in range(3):
-                blocks[x].append([*range(1, 10)])
+                blocks[x].append({*range(1, 10)})
         for x in range(9):
             for y in range(9):
                 self.cells.append(Cell(
