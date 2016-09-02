62c0b335245d6695072d8c8a8b4b1b76023345f1
Hendrik Werner <hendrik.to@gmail.com>
2016-09-03 01:15:03 +0200

    Mark variables as private.

--- model/Cell.py
+++ model/Cell.py
@@ -3,32 +3,32 @@ class Cell:
     allowed = range(1, 10)
 
     def __init__(self, sudoku, row, column, block):
-        self.value = 0
-        self.sudoku = sudoku
-        self.row = row
-        self.column = column
-        self.block = block
+        self.__value = 0
+        self.__sudoku = sudoku
+        self.__row = row
+        self.__column = column
+        self.__block = block
         sudoku.empty.append(self)
 
     def set_value(self, new_value):
         assert new_value in self.allowed
         self.clear()
-        self.value = new_value
-        self.row.remove(self.value)
-        self.column.remove(self.value)
-        self.block.remove(self.value)
-        self.sudoku.empty.remove(self)
+        self.__value = new_value
+        self.__row.remove(self.__value)
+        self.__column.remove(self.__value)
+        self.__block.remove(self.__value)
+        self.__sudoku.empty.remove(self)
 
     def clear(self):
-        if self.value:
-            self.row.add(self.value)
-            self.column.add(self.value)
-            self.block.add(self.value)
-            self.value = 0
-            self.sudoku.empty.append(self)
+        if self.__value:
+            self.__row.add(self.__value)
+            self.__column.add(self.__value)
+            self.__block.add(self.__value)
+            self.__value = 0
+            self.__sudoku.empty.append(self)
 
     def get_candidates(self):
-        return self.row & self.column & self.block
+        return self.__row & self.__column & self.__block
 
     def __str__(self):
-        return str(self.value)
+        return str(self.__value)
