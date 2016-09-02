d0f469a7d1920c7c3e418c659399d05dae621bd9
Hendrikto <hendrik.to@gmail.com>
2016-09-02 23:15:00 +0200

    Implement solving via backtracking.

--- model/SudokuSolver.py
+++ model/SudokuSolver.py
@@ -2,3 +2,14 @@
 class SudokuSolver:
     def __init__(self, sudoku):
         self.sudoku = sudoku
+
+    def solve(self):
+        if len(self.sudoku.empty) == 0:
+            return True
+        cell = self.sudoku.empty[0]
+        for candidate in cell.get_candidates():
+            cell.set_value(candidate)
+            if self.solve():
+                return True
+        cell.clear()
+        return False
