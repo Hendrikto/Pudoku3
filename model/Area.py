bb983098ef0a65196f86d93b9f5ab888a5741134
Hendrik Werner <hendrik.to@gmail.com>
2016-09-04 03:58:11 +0200

    Add a docstring.

--- model/Area.py
+++ model/Area.py
@@ -1,5 +1,10 @@
 # author: Hendrik Werner
 class Area(set):
+    """
+    A set of integers that is initialized to contain all legal values for a
+    Sudoku (1..9).
+    """
+
     def __init__(self):
         super().__init__()
         self.update(range(1, 10))
