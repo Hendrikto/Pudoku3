8a56949abc4807332c571aca1a1edbe7ffaaaf43
Hendrikto <hendrik.to@gmail.com>
2016-09-08 14:13:22 +0200

    Add a grid to improve readability.

--- model/PrettyStringifier.py
+++ model/PrettyStringifier.py
@@ -6,6 +6,14 @@ class PrettyStringifier(SudokuStringifier):
     def stringify(self, sudoku):
         s = ""
         for i in range(81):
+            cellID = i + 1
+            if cellID != 1 and cellID % 27 == 1:
+                s += "------+-------+------\n"
             s += str(sudoku.cells[i])
-            s += "\n" if (i + 1) % 9 == 0 else " "
+            if cellID % 9 == 0:
+                s += "\n"
+            elif cellID % 3 == 0:
+                s += " | "
+            else:
+                s += " "
         return s
