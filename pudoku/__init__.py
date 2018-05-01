8eec04ce06a38f028dd906b8bbf77135850b2685
Hendrikto <hendrik.to@gmail.com>
2018-05-01 23:31:29 +0200

    Initialize the module properly

--- pudoku/__init__.py
+++ pudoku/__init__.py
@@ -1,1 +1,7 @@
-# author: Hendrik Werner
+from .PrettyStringifier import PrettyStringifier
+from .SimpleStringifier import SimpleStringifier
+from .Sudoku import Sudoku
+from .SudokuSolver import SudokuSolver
+
+
+__author__ = "Hendrik Werner"
