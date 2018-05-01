a5598b07918c0bd4a54d49eb82a1a03ac62c317a
Hendrikto <hendrik.to@gmail.com>
2018-05-02 00:09:12 +0200

    Improve stringification with box drawing characters

--- pudoku/PrettyStringifier.py
+++ pudoku/PrettyStringifier.py
@@ -8,12 +8,12 @@ class PrettyStringifier(SudokuStringifier):
         for i in range(81):
             cellID = i + 1
             if cellID != 1 and cellID % 27 == 1:
-                s += "------+-------+------\n"
+                s += "──────┼───────┼──────\n"
             s += str(sudoku.cells[i])
             if cellID % 9 == 0:
                 s += "\n"
             elif cellID % 3 == 0:
-                s += " | "
+                s += " │ "
             else:
                 s += " "
         return s
